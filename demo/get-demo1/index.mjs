290fed6957d29d5efbfa3948c54dc18e970824fc
Owen Buckley <owenbuckley13@gmail.com>
2022-05-14 13:06:45 -0400

    co-locate header

--- demo/get-demo1/index.mjs
+++ demo/get-demo1/index.mjs
@@ -2,7 +2,7 @@
 import { renderToString } from 'wc-compiler';
 
 export async function handler () {
-  const { html } = await renderToString(new URL('../shared/header.component.js', import.meta.url));
+  const { html } = await renderToString(new URL('./header.component.js', import.meta.url));
 
   return {
     statusCode: 200,
